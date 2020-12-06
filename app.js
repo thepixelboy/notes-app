b2c741c2e8c9e3f1a6938a28a12845fb93baf74b
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-06 18:47:10 +0100

    Printing in Color

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const validator = require('validator');
+const chalk = require('chalk');
 const getNotes = require('./notes');
 
 const notes = getNotes();
 console.log(notes);
 
-console.log(validator.isURL('https://github.com'));
+console.log(chalk.green('Success!'));
+console.log(chalk.green.bold('Success! (Bold)'));
+console.log(chalk.green.bold.inverse('Success! (Bold & Inverse)'));
