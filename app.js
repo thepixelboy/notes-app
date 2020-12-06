49149395b896158be9061c9f84ecb25bf29694b9
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-06 18:17:22 +0100

    Importing npm Modules

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const validator = require('validator');
 const getNotes = require('./notes');
 
 const notes = getNotes();
-
 console.log(notes);
+
+console.log(validator.isURL('https://github.com'));
