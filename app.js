44b2954056a5d04d63566f216e7e4118024deea8
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-06 19:04:43 +0100

    Global npm Modules and nodemon

--- app.js
+++ app.js
@@ -4,6 +4,4 @@ const getNotes = require('./notes');
 const notes = getNotes();
 console.log(notes);
 
-console.log(chalk.green('Success!'));
-console.log(chalk.green.bold('Success! (Bold)'));
-console.log(chalk.green.bold.inverse('Success! (Bold & Inverse)'));
+console.log(chalk.blue.bold.inverse('Success!'));
