206403d460877612f55a7be26a7872f465f2ba38
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-06 17:44:03 +0100

    Importing Your Own Files

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-const fs = require('fs');
+const getNotes = require('./notes');
 
-// fs.writeFileSync('notes.txt', 'My name is Ruben.');
+const notes = getNotes();
 
-fs.appendFileSync(
-  'notes.txt',
-  `\n\nThis is me appending text to notes.txt using appendFileSync.`
-);
-
-//
-// Challenge: Append a message to notes.txt
-//
-// 1. Use appendFileSync to append to the file
-// 2. Run the script
-// 3. Check your work by opening the file and viewing the appended text
+console.log(notes);
