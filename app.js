dbac0c6c106444b2a3048647d9ef0a7ceb1278e6
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-07 11:07:08 +0100

    Getting Input from Users

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const chalk = require('chalk');
 const getNotes = require('./notes');
 
-const notes = getNotes();
-console.log(notes);
+const command = process.argv[2];
 
-console.log(chalk.blue.bold.inverse('Success!'));
+console.log(process.argv);
+
+if (command === 'add') {
+  console.log('Adding note...');
+} else if (command === 'remove') {
+  console.log('Removing note...');
+}
