9b2029269debd46bbbb8cf568f5064c145000032
Ruben <34570952+thepixelboy@users.noreply.github.com>
2020-12-07 18:51:11 +0100

    Refactoring to Use Arrow Functions

--- notes.js
+++ notes.js
@@ -1,15 +1,13 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = function () {
+const getNotes = () => {
   return 'Your notes...';
 };
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
   const notes = loadNotes();
-  const duplicateNotes = notes.filter(function (note) {
-    return note.title === title;
-  });
+  const duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length === 0) {
     notes.push({
@@ -23,32 +21,30 @@ const addNote = function (title, body) {
   }
 };
 
-const saveNotes = function (notes) {
-  const dataJSON = JSON.stringify(notes);
-  fs.writeFileSync('notes.json', dataJSON);
-};
-
-const loadNotes = function () {
-  try {
-    const dataBuffer = fs.readFileSync('notes.json');
-    const dataJSON = dataBuffer.toString();
-    return JSON.parse(dataJSON);
-  } catch (err) {
-    return [];
-  }
-};
-
-const removeNote = function (title) {
+const removeNote = (title) => {
   const notes = loadNotes();
-  const notesToKeep = notes.filter(function (note) {
-    return note.title !== title;
-  });
+  const notesToKeep = notes.filter((note) => note.title !== title);
 
   if (notesToKeep.length === notes.length) {
     console.log(chalk.red.inverse('No note found!'));
   } else {
     saveNotes(notesToKeep);
     console.log(chalk.green.inverse('Note removed!'));
+  }
+};
+
+const saveNotes = (notes) => {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = () => {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (err) {
+    return [];
   }
 };
 
